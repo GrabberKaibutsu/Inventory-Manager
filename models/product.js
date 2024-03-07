b9daa8e31dd8dca30641b9725840894569e9e29e
Carson Holder <holderc2004@gmail.com>
2024-03-07 17:42:29 -0500

    able to show indes page

---
+++ models/product.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    count: {type:Number},
+    isLow: {type: Boolean},
+    user: { type: mongoose.Types.ObjectId, ref: 'User'}
+})
+
+
+module.exports = mongoose.model("Product", productSchema)
--- server.js
+++ server.js
@@ -7,8 +7,17 @@ const methodOverride = require('method-override')
 const morgan = require('morgan')
 const session = require('express-session')
 const PORT = process.env.PORT || 5000
-
+const proCtrl = require('./controllers/productController')
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// app.get('/', function (req, res) {
+//     // res.redirect('/')
+//   });
+
+  app.use('/', proCtrl)
 
 app.listen(PORT, () => {
     console.log('Open on port', PORT)
